f9b55cc8eb5c33aa61e7ad9efa149f004777115d
financenick <mnick.direct@gmail.com>
2023-07-15 01:48:10 +0400

    ver2

--- WF1/Form1.cs
+++ WF1/Form1.cs
@@ -63,10 +63,22 @@ namespace WF1
             }
         }
 
-        private Dictionary<string, int> CalculateItemCount(string fileName)
+        private Dictionary<string, int[]> CalculateDocumentCounts(string FileName, bool isRKK)
         {
-            Dictionary<string, int> itemCounts = new Dictionary<string, int>();
+            Dictionary<string, int[]> documentCounts = new Dictionary<string, int[]>();
+            
 
+            // Обработка файла РКК
+            ProcessFile(FileName, documentCounts, true);
+
+            // Обработка файла обращений
+            ProcessFile(FileName, documentCounts, false);
+
+            return documentCounts;
+        }
+
+        private void ProcessFile(string fileName, Dictionary<string, int[]> documentCounts, bool isRKK)
+        {
             using (StreamReader reader = new StreamReader(fileName))
             {
                 while (!reader.EndOfStream)
@@ -77,31 +89,47 @@ namespace WF1
                         string[] columns = line.Split('\t');
                         if (columns.Length >= 2)
                         {
-                            string leader = columns[0];
-                            string performers = columns[1];
+                            string leader = columns[0].Trim();
+                            string performers = columns[1].Trim();
 
-                            string responsiblePerformer = (leader == "Климов Сергей Александрович") ? performers.Split(',')[0] : leader;
+                            // Приведение формата имени лидера
+                            string[] leaderParts = leader.Split(' ');
+                            string leaderFormatted = leaderParts[0] + " " + leaderParts[1][0] + "." + leaderParts[2][0] + ".";
 
-                            if (itemCounts.ContainsKey(responsiblePerformer))
-                                itemCounts[responsiblePerformer]++;
+                            // Определение ответственного исполнителя
+                            string responsiblePerformer = (leaderFormatted.Equals("Климов С.А.")) ? performers.Split(';')[0].Trim() : leaderFormatted;
+
+                           
+                            responsiblePerformer = responsiblePerformer.Replace(" (Отв.)", "");
+
+                            // Обновление счетчиков
+                            if (documentCounts.ContainsKey(responsiblePerformer))
+                            {
+                                if (isRKK)
+                                    documentCounts[responsiblePerformer][0]++; // РКК
+                                else
+                                    documentCounts[responsiblePerformer][1]++; // Обращения
+
+                                documentCounts[responsiblePerformer][2]++; // Суммарно документов
+                            }
                             else
-                                itemCounts[responsiblePerformer] = 1;
+                            {
+                                documentCounts[responsiblePerformer] = new int[] { isRKK ? 1 : 0, isRKK ? 0 : 1, 1 };
+                            }
                         }
                     }
                 }
             }
-
-            return itemCounts;
         }
 
-        private Dictionary<string, int> CalculateTotalCounts(Dictionary<string, int> rkkCounts, Dictionary<string, int> requestsCounts)
+        private Dictionary<string, int> CalculateTotalCounts(Dictionary<string, int[]> rkkCounts, Dictionary<string, int[]> requestsCounts)
         {
             Dictionary<string, int> totalCounts = new Dictionary<string, int>();
 
             foreach (string performer in rkkCounts.Keys)
             {
-                int rkkCount = rkkCounts[performer];
-                int requestsCount = requestsCounts.ContainsKey(performer) ? requestsCounts[performer] : 0;
+                int rkkCount = rkkCounts[performer][0];
+                int requestsCount = requestsCounts.ContainsKey(performer) ? requestsCounts[performer][1] : 0;
                 int totalCount = rkkCount + requestsCount;
 
                 totalCounts[performer] = totalCount;
@@ -111,7 +139,7 @@ namespace WF1
             {
                 if (!totalCounts.ContainsKey(performer))
                 {
-                    int requestsCount = requestsCounts[performer];
+                    int requestsCount = requestsCounts[performer][1];
                     totalCounts[performer] = requestsCount;
                 }
             }
@@ -129,27 +157,23 @@ namespace WF1
 
             try
             {
-                Dictionary<string, int> rkkCounts = CalculateItemCount(rkkFileName);
-                Dictionary<string, int> requestsCounts = CalculateItemCount(requestsFileName);
+                Dictionary<string, int[]> rkkCounts = CalculateDocumentCounts(rkkFileName, true);
+                Dictionary<string, int[]> requestsCounts = CalculateDocumentCounts(requestsFileName, false);
                 Dictionary<string, int> totalCounts = CalculateTotalCounts(rkkCounts, requestsCounts);
 
-                //int totalDocuments = totalCounts.Values.Sum();
-                //int rkkDocuments = rkkCounts.Values.Sum();
-                //int requestsDocuments = requestsCounts.Values.Sum();
-
                 totalDocuments = totalCounts.Values.Sum();
-                rkkDocuments = rkkCounts.Values.Sum();
-                requestsDocuments = requestsCounts.Values.Sum();
+                rkkDocuments = rkkCounts.Values.Sum(x => x[0]);
+                requestsDocuments = requestsCounts.Values.Sum(x => x[1]);
 
                 MessageBox.Show($"Документов РКК: {rkkDocuments}\nДокументов обращений: {requestsDocuments}\nВсего документов: {totalDocuments}", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                // Создаем таблицу DataGridView с пятью колонками: номер, исполнитель, РКК, обращения, сумма
+                
                 DataGridView dataGridView = new DataGridView();
                 dataGridView.Dock = DockStyle.Fill;
                 dataGridView.AutoGenerateColumns = true;
                 dataGridView.RowHeadersVisible = true;
 
-                // Добавляем колонку с номерами
+                
                 dataGridView.Columns.Add(new DataGridViewTextBoxColumn()
                 {
                     HeaderText = "№",
@@ -159,44 +183,44 @@ namespace WF1
                     AutoSizeMode = DataGridViewAutoSizeColumnMode.None
                 });
 
-                // Добавляем колонку с именем исполнителя
+             
                 dataGridView.Columns.Add(new DataGridViewTextBoxColumn()
                 {
                     Name = "performerColumn",
                     HeaderText = "Исполнитель"
                 });
 
-                // Добавляем колонку с данными по РКК
+               
                 DataGridViewTextBoxColumn rkkColumn = new DataGridViewTextBoxColumn();
                 rkkColumn.DataPropertyName = "RKK";
                 rkkColumn.HeaderText = "РКК";
                 dataGridView.Columns.Add(rkkColumn);
 
-                // Добавляем колонку с данными по обращениям
+                
                 DataGridViewTextBoxColumn requestsColumn = new DataGridViewTextBoxColumn();
                 requestsColumn.DataPropertyName = "Requests";
                 requestsColumn.HeaderText = "Обращения";
                 dataGridView.Columns.Add(requestsColumn);
 
-                // Добавляем колонку с суммой
+              
                 DataGridViewTextBoxColumn totalColumn = new DataGridViewTextBoxColumn();
                 totalColumn.DataPropertyName = "Total";
                 totalColumn.HeaderText = "Сумма";
                 dataGridView.Columns.Add(totalColumn);
 
-                // Заполняем таблицу данными
+             
                 int rowNumber = 1;
-                foreach (string performer in rkkCounts.Keys)
+                foreach (string performer in totalCounts.Keys)
                 {
-                    int rkkCount = rkkCounts[performer];
-                    int requestsCount = requestsCounts.ContainsKey(performer) ? requestsCounts[performer] : 0;
-                    int totalCount = totalCounts.ContainsKey(performer) ? totalCounts[performer] : 0;
+                    int rkkCount = rkkCounts.ContainsKey(performer) ? rkkCounts[performer][0] : 0;
+                    int requestsCount = requestsCounts.ContainsKey(performer) ? requestsCounts[performer][1] : 0;
+                    int totalCount = totalCounts[performer];
 
                     dataGridView.Rows.Add(rowNumber, performer, rkkCount, requestsCount, totalCount);
                     rowNumber++;
                 }
 
-                // Фиксированная нумерация строк при сортировке
+          
                 dataGridView.CellFormatting += (cellSender, cellFormattingEventArgs) =>
                 {
                     if (cellFormattingEventArgs.ColumnIndex == 0 && cellFormattingEventArgs.RowIndex >= 0)
@@ -205,7 +229,7 @@ namespace WF1
                     }
                 };
 
-                // Создаем новую форму и отображаем таблицу на ней
+          
                 Form resultForm = new Form();
                 resultForm.Width = 800;
                 resultForm.Height = 800;
@@ -213,7 +237,7 @@ namespace WF1
 
                 resultForm.Controls.Add(dataGridView);
 
-                // Добавляем кнопку сохранения в файл
+                //Кнопка сохранения в файл
                 Button saveButton = new Button();
                 saveButton.Text = "Сохранить в файл";
                 saveButton.Dock = DockStyle.Bottom;
@@ -227,6 +251,7 @@ namespace WF1
                 MessageBox.Show($"Ошибка при обработке файлов: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
 
 
         private void SaveToWordFile(DataGridView dataGridView)
@@ -243,33 +268,28 @@ namespace WF1
                     mainPart.Document = new Document();
                     DocumentFormat.OpenXml.Wordprocessing.Body body = mainPart.Document.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Body());
 
-                    // Добавление заголовка документа
+                   
                     Paragraph heading = new Paragraph(
                         new ParagraphProperties(new Justification { Val = JustificationValues.Center }),
                         new Run(new DocumentFormat.OpenXml.Wordprocessing.Text("Справка о неисполненных документах и обращениях граждан"))
                     );
                     body.AppendChild(heading);
 
-                    // Добавление информации о неисполненных документах и обращениях граждан
-                    // Получение количества документов РКК
-                    //int rkkDocuments = rkkCounts.Sum(pair => pair.Value);
-                    //// Получение количества документов обращений
-                    //int requestsDocuments = requestsCounts.Sum(pair => pair.Value);
-                    //// Получение общего количества документов
-                    //int totalDocuments = totalCounts.Sum(pair => pair.Value);
-
+                    
                     Paragraph info = new Paragraph(
                         new Run(new DocumentFormat.OpenXml.Wordprocessing.Text($"Не исполнено в срок {totalDocuments} документов, из них:")),
-                        new Run(new DocumentFormat.OpenXml.Wordprocessing.Text($"\n- количество неисполненных входящих документов: {rkkDocuments}")),
-                        new Run(new DocumentFormat.OpenXml.Wordprocessing.Text($"\n- количество неисполненных письменных обращений граждан: {requestsDocuments}"))
+                        new Break(),
+                        new Run(new RunProperties(new RunFonts { Ascii = "Arial" }), new DocumentFormat.OpenXml.Wordprocessing.Text($"\n- количество неисполненных входящих документов: {rkkDocuments}")),
+                        new Break(),
+                        new Run(new RunProperties(new RunFonts { Ascii = "Arial" }), new DocumentFormat.OpenXml.Wordprocessing.Text($"\n- количество неисполненных письменных обращений граждан: {requestsDocuments}"))
                     );
+
                     body.AppendChild(info);
 
-
-                    // Создание таблицы в документе Word
+  
                     Table table = new Table();
 
-                    // Добавление стилей таблицы
+                  
                     TableProperties tableProperties = new TableProperties(
                         new TableBorders(
                             new TopBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 },
@@ -282,7 +302,7 @@ namespace WF1
                     );
                     table.AppendChild(tableProperties);
 
-                    // Добавление заголовка таблицы
+                    
                     TableRow headerRow = new TableRow();
 
                     headerRow.AppendChild(new TableCell(new Paragraph(new Run(new DocumentFormat.OpenXml.Wordprocessing.Text("№"))))); // Добавление ячейки с заголовком номера строки
@@ -296,7 +316,7 @@ namespace WF1
 
                     table.AppendChild(headerRow);
 
-                    int rowNumber = 1; // Переменная счетчика для номеров строк
+                    int rowNumber = 1; 
 
                     foreach (DataGridViewRow dataRow in dataGridView.Rows)
                     {
@@ -305,29 +325,49 @@ namespace WF1
 
                         for (int i = 0; i < dataRow.Cells.Count; i++)
                         {
-                            // Пропустить второй столбец
+                           
                             if (i != 0)
                             {
                                 string cellValue = dataRow.Cells[i].Value?.ToString() ?? string.Empty;
 
-                                TableCell tableCell = new TableCell(
-                                    new Paragraph(
-                                        new ParagraphProperties(new Justification { Val = JustificationValues.Center }),
-                                        new Run(
-                                            new RunProperties(new DocumentFormat.OpenXml.Wordprocessing.FontSize { Val = "14" }),
-                                            new DocumentFormat.OpenXml.Wordprocessing.Text(cellValue)
-                                        )
+                                TableCell tableCell = new TableCell();
+
+                                Paragraph paragraph = new Paragraph(
+                                    new ParagraphProperties(
+                                        new SpacingBetweenLines { After = "0" },
+                                        new Justification { Val = JustificationValues.Center }
+                                    ),
+                                    new Run(
+                                        new RunProperties(
+                                            new RunFonts { Ascii = "Arial" },
+                                            new FontSize { Val = "14" }
+                                        ),
+                                        new DocumentFormat.OpenXml.Wordprocessing.Text(cellValue)
                                     )
                                 );
+
+                                tableCell.AppendChild(paragraph);
                                 tableRow.AppendChild(tableCell);
                             }
                         }
 
                         table.AppendChild(tableRow);
-                        rowNumber++; // Увеличение счетчика номеров строк
+                        rowNumber++;
                     }
 
                     body.AppendChild(table);
+
+
+                    // Дата запуска программы
+                    DateTime currentDate = DateTime.Now;
+                    string formattedDate = currentDate.ToString("dd.MM.yyyy");
+
+                    Paragraph dateParagraph = new Paragraph(
+                        new Run(new DocumentFormat.OpenXml.Wordprocessing.Text("Дата составления справки:\t" + formattedDate))
+                    );
+
+                    body.AppendChild(dateParagraph);
+
                 }
 
                 MessageBox.Show("Файл успешно сохранен.", "Сохранение", MessageBoxButtons.OK, MessageBoxIcon.Information);
